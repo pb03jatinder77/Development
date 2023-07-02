a6655bf93af32564f566cf0cc5598ad2fb5130dc
Jatinder <jatinder17966@gmail.com>
2023-07-02 11:52:00 -0400

    set html file

--- app.js
+++ app.js
@@ -4,15 +4,21 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
-
+const exphbs  = require('express-handlebars');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
 
 // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
 // Set the public file path
 
 app.use(logger('dev'));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  let baseUrl = path.join(__dirname, '../public');
+  res.render('index',{baseUrl});
 });
 
 module.exports = router;
