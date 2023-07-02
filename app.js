41f049e52acfb7807f3065ffb93909c2f2a7214e
Jatinder <jatinder17966@gmail.com>
2023-07-02 04:13:00 -0400

    weather

--- app.js
+++ app.js
@@ -1,26 +1,44 @@
-const { getIPAddress } = require('./src/GetIpAddress');
-const { getLoaction } = require('./src/GetLocation');
-const { getWeather } = require('./src/GetWeather');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
-getIPAddress(null,(error, ipAddress) => {
-    if (error) {
-        console.error('Error getting IP address:', error);
-    } else {
-        console.log('ipAddress: ', ipAddress);
-        getLoaction(ipAddress, (error, location) => {
-            if (error) {
-                console.error('Error getting geolocation data:', error);
-            } else {
-                console.log('Location: ', location?.data?.city);
-                const city = location?.data?.city
-                getWeather(city, (error, Weather) => {
-                    if (error) {
-                        console.error('Error getting weather data:', error);
-                    } else {
-                        console.log('Weather: ', Weather);
-                    }
-                });
-            }
-        });
-    }
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+// Set the public file path
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
--- src/GetLocation.js
+++ src/GetLocation.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 /** get your Loaction */
 exports.getLoaction = async (request, result) => {
     try {
-      const response =  await axios.get(`http://ip-api.com/json/${request}`);
+      const response =  await axios.get(`http://ip-api.com/json/${request}?format=json`);
       result(null, response);
     } catch (err) {
       result(err, null);
